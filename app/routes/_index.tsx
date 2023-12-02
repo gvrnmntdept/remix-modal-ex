673ae2b55cc2e68e51bb9fdf282ef876326ee45c
Vsevolod Filinov <filinov.vsevolod@gmail.com>
2023-12-02 17:59:01 +0300

    Add Demo Dialog

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,14 @@
 import type { MetaFunction } from "@remix-run/node";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+import { Button } from "~/components/ui/button";
+import { useLocation, useNavigate } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +17,63 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const TEST_DIALOG_ID = "TEST-DIALOG-ID";
+
 export default function Index() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const open = location.state?.dialogID === TEST_DIALOG_ID;
+
+  const handeOpenChange = (newOpen: boolean) => {
+    if (newOpen) {
+      navigate("/", {
+        replace: false,
+        relative: "route",
+        state: { dialogID: TEST_DIALOG_ID },
+      });
+    } else {
+      navigate(-1);
+    }
+  };
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="w-full h-screen flex items-center justify-center bg-background text-foreground">
+      <div className="max-w-md">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Dialog Navigation
+        </h1>
+        <p className="leading-7 [&:not(:first-child)]:mt-6 mb-8">
+          This dialog is integrated with browser's session history stack. Try
+          using back and forward buttons.
+        </p>
+        <Dialog open={open} onOpenChange={handeOpenChange}>
+          <DialogTrigger asChild>
+            <Button size="lg" className="min-w-full">
+              Open
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>This is a test</DialogTitle>
+              <DialogDescription>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                sagittis at erat quis vestibulum. Nam feugiat turpis pretium,
+                tempor lectus sed, tempus ante. Nullam vitae dui eros. Nunc vel
+                tortor sit amet libero vulputate cursus ut in dui. Duis
+                tincidunt tempor ex, eget accumsan eros tincidunt volutpat. Nam
+                varius dui sed elit fermentum, nec ultrices neque gravida. Ut
+                placerat volutpat nunc eu posuere. Nunc rhoncus elit at arcu
+                elementum hendrerit. Proin viverra in ligula nec volutpat.
+                Vestibulum quam enim, luctus vitae sollicitudin eu, egestas non
+                justo. Mauris tincidunt iaculis velit. Aenean ut interdum ipsum,
+                sit amet gravida sapien. Fusce nec consectetur eros, in
+                imperdiet tellus. Curabitur finibus purus ac aliquet vehicula.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </div>
     </div>
   );
 }
